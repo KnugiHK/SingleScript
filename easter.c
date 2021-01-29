214c15082567d9280cf010b23f1fe925efcafa75
Knugi <24708955+KnugiHK@users.noreply.github.com>
2021-01-29 08:16:20 +0000

    Update easter.c

--- easter.c
+++ easter.c
@@ -3,20 +3,20 @@
 // Based on: https://github.com/dateutil/dateutil/blob/master/dateutil/easter.py
 
 int main(){
-    int g, e, y, h, i, j, p, d, m, c;
+    int y;
     printf("Enter year: ");
     scanf("%d", &y);
-    g = y % 19;
-    e = 0;
+    int g = y % 19;
+    int e = 0;
 
-    c = y / 100;
-    h = (c - c / 4 - (8 * c + 13) / 25 + 19 * g + 15) % 30;
-    i = h - (h / 28)*(1 - (h /28)*(29 / (h + 1))*((21 - g) / 11));
-    j = (y + y / 4 + i + 2 - c + c / 4) % 7;
+    int c = y / 100;
+    int h = (c - c / 4 - (8 * c + 13) / 25 + 19 * g + 15) % 30;
+    int i = h - (h / 28) * (1 - (h /28)*(29 / (h + 1)) * ((21 - g) / 11));
+    int j = (y + y / 4 + i + 2 - c + c / 4) % 7;
 
-    p = i - j + e;
-    d = 1 + (p + 27 + (p + 6) / 40) % 31;
-    m = 3 + (p + 26) / 30;
+    int p = i - j + e;
+    int d = 1 + (p + 27 + (p + 6) / 40) % 31;
+    int m = 3 + (p + 26) / 30;
     if (m == 4){
         printf("April %d\n", d);
     }else if (m == 3){
