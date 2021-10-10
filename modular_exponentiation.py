f99dd245d458753d00deb2813eac5138c7620486
Knugi <24708955+KnugiHK@users.noreply.github.com>
2021-10-10 18:19:06 +0000

    Create modular_exponentiation.py

---
+++ modular_exponentiation.py
@@ -0,0 +1,24 @@
+def fast_expo(b, e, n):
+    seq = bin(e)[3:]
+    result = b
+    for element in seq:
+        result = pow(result, 2)
+        if element == "1":
+            result *= b
+        result %= n
+    return result
+
+
+result = fast_expo(
+    123456789123456789,
+    123456789123456789123456789123456789,
+    123456789123456789123456789123456789123456789
+)
+print(
+    r == pow(
+        123456789123456789,
+        123456789123456789123456789123456789,
+        123456789123456789123456789123456789123456789
+    )
+)
+print(r)
